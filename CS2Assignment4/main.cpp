86bbb992f9e2832266a42e2bf0f87d761568b5e8
Jen <jenniferballing@gmail.com>
2014-02-14 18:43:38 -0700

    Added filestream

--- CS2Assignment4/main.cpp
+++ CS2Assignment4/main.cpp
@@ -2,9 +2,93 @@
 #include "Person.h"
 #include "OvernightPackage.h"
 #include "TwoDayPackage.h"
+#include <fstream>
 
+void labelFunc(Person s, Person r, string cost);
+void fileInputFunc ();
 int main()
 {
+    int pause=0;
+    fileInputFunc();
 
+    cin>>pause;
     return 0;
+}
+
+void labelFunc(Person s, Person r, string cost)
+{
+    string StempN=s.getName();
+    string StempA=s.getAdd();
+    string StempC=s.getCity();
+    string StempS=s.getState();
+    string StempZ=s.getZip();
+
+    string RtempN=r.getName();
+    string RtempA=r.getAdd();
+    string RtempC=r.getCity();
+    string RtempS=r.getState();
+    string RtempZ=r.getZip();
+
+    cout<<"Testing, testing..."<<endl;
+    cout<<StempN<<endl;
+    cout<<StempA<<endl;
+    cout<<StempC<<endl;
+    cout<<StempS<<endl;
+    cout<<StempZ<<endl;
+    cout<<RtempN<<endl;
+    cout<<RtempA<<endl;
+    cout<<RtempC<<endl;
+    cout<<RtempS<<endl;
+    cout<<RtempZ<<endl;
+    
+    cout<<"Sender: "<<StempN <<endl;
+    cout<<StempN<<"\n"<<StempC<<", "<<StempS<<" "<<StempZ<<endl<<endl;
+
+    cout<<"Recipient: "<<RtempN <<endl;
+    cout<<RtempA<<"\n"<<RtempC<<", "<<RtempS<<" "<<RtempZ<<endl<<endl;
+
+    cout<<"Delivery Cost: "<< cost;    
+}
+void fileInputFunc ()
+{
+    int pause, weight;
+    string pkgType, sName, sAdd, sCity, sState, sZip;
+    string rName, rAdd, rCity, rState, rZip;
+    double costPerOz, addPerOz, addFlatRate;
+    Person send, receive;
+    
+    ifstream fin ("package.txt");
+    
+    fin>> pkgType;
+    if( pkgType == "p" || pkgType  == "P")
+    {
+        fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip;
+    }
+    else if( pkgType == "O" || pkgType  == "o")
+    {
+        fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip>>addFlatRate;
+    }
+    else if( pkgType == "T" || pkgType  == "t")
+    {
+        fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip>>addPerOz;
+    }
+
+   /* cout<<weight<<costPerOz<<sName<< sAdd<<sCity<<sState<< sZip<< rName<< rAdd << rCity<<rState<< rZip;*/
+
+    send.setName(sName);
+    send.setAdd(sAdd);
+    send.setCity(sCity);
+    send.setState(sState);
+    send.setZip(sZip);
+
+    receive.setName(rName);
+    receive.setAdd(rAdd);
+    receive.setCity(rCity);
+    receive.setState(rState);
+    receive.setZip(rZip);
+
+   /* cout<<"Testing again..."<<send.getName()<<send.getAdd()<<send.getCity()<<send.getCity()<<send.getState()<<send.getZip();*/
+
+    string cost="6*5=30";
+    labelFunc(send, receive, cost);
 }
