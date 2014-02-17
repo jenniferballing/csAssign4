b1e42d15fe8c2949e58596e6a9d0346ae6cbfa6e
Jen <jenniferballing@gmail.com>
2014-02-17 14:54:10 -0700

    Done! ...Maybe
    All is working as I believe it should. Needs testing.

--- CS2Assignment4/main.cpp
+++ CS2Assignment4/main.cpp
@@ -3,19 +3,21 @@
 #include "OvernightPackage.h"
 #include "TwoDayPackage.h"
 #include <fstream>
+#include <iomanip>
 
-void labelFunc(Person &s, Person &r, string cost);
+void labelFunc(Person &s, Person &r, double cost);
 void fileInputFunc ();
+
 int main()
 {
-    int pause=0;
     fileInputFunc();
-
+    
+    int pause=0;
     cin>>pause;
     return 0;
 }
 
-void labelFunc(Person &s, Person &r, string cost)
+void labelFunc(Person &s, Person &r, double cost)
 {
     string StempN=s.getName();
     string StempA=s.getAdd();
@@ -29,67 +31,64 @@ void labelFunc(Person &s, Person &r, string cost)
     string RtempS=r.getState();
     string RtempZ=r.getZip();
 
-    cout<<"Testing, testing..."<<endl;
+    cout<<"***SHIPPING LABEL***"<<endl<<endl;
+    cout<<"Delivery Cost: $"<< fixed<<setprecision(2)<< cost<< endl<<endl<<endl;
     
-    cout<<StempA<<endl;
-    cout<<StempC<<endl;
-    cout<<StempS<<endl;
-    cout<<StempZ<<endl;
-    cout<<RtempN<<endl;
+    cout<<"FROM: "<<endl;
+    cout<<StempN <<endl;
+    cout<<StempA<<"\n"<<StempC<<", "<<StempS<<" "<<StempZ<<endl<<endl<<endl;
+
+    cout<<"SHIP TO: "<<endl;
+    cout<<RtempN <<endl;
     cout<<RtempA<<endl;
-    cout<<RtempC<<endl;
-    cout<<RtempS<<endl;
-    cout<<RtempZ<<endl;
-    
-    cout<<"Sender: "<<StempN <<endl;
-    cout<<StempN<<"\n"<<StempC<<", "<<StempS<<" "<<StempZ<<endl<<endl;
-
-    cout<<"Recipient: "<<RtempN <<endl;
-    cout<<RtempA<<"\n"<<RtempC<<", "<<RtempS<<" "<<RtempZ<<endl<<endl;
-
-    cout<<"Delivery Cost: "<< cost;    
+    cout<<RtempC<<", "<<RtempS<<" "<<RtempZ<<endl<<endl<<endl<<endl;    
 }
 void fileInputFunc ()
 {
     int pause, weight;
     string pkgType, sName, sAdd, sCity, sState, sZip;
     string rName, rAdd, rCity, rState, rZip;
-    double costPerOz, addPerOz, addFlatRate;
+    double costPerOz, addPerOz, addFlatRate, costToShip;
     Person send, receive;
     
     ifstream fin ("package.txt");
-    
     fin>> pkgType;
-    if( pkgType == "p" || pkgType  == "P")
-    {
-        fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip;
+    while(!fin.eof())
+    {        
+        if( pkgType == "p" || pkgType  == "P")
+        {
+            fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip;
+            costToShip=weight*costPerOz;
+        }
+        else if( pkgType == "O" || pkgType  == "o")
+        {
+            fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip>>addFlatRate;
+            costToShip=weight*costPerOz+addFlatRate;
+        }
+        else if( pkgType == "T" || pkgType  == "t")
+        {
+            fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip>>addPerOz;
+            costToShip=weight*(costPerOz+addPerOz);
+        }
+
+        send.setName(sName);
+        send.setAdd(sAdd);
+        send.setCity(sCity);
+        send.setState(sState);
+        send.setZip(sZip);
+
+        receive.setName(rName);
+        receive.setAdd(rAdd);
+        receive.setCity(rCity);
+        receive.setState(rState);
+        receive.setZip(rZip);
+
+        Person* sendPtr=new Person;
+        Person* receivePtr= new Person;
+        sendPtr=&send;
+        receivePtr=&receive;
+
+        labelFunc(send, receive, costToShip);
+        fin>>pkgType; 
     }
-    else if( pkgType == "O" || pkgType  == "o")
-    {
-        fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip>>addFlatRate;
-    }
-    else if( pkgType == "T" || pkgType  == "t")
-    {
-        fin>>weight>>costPerOz>>sName>> sAdd>> sCity>> sState>> sZip>> rName>> rAdd >> rCity>> rState>> rZip>>addPerOz;
-    }
-
-    send.setName(sName);
-    send.setAdd(sAdd);
-    send.setCity(sCity);
-    send.setState(sState);
-    send.setZip(sZip);
-
-    receive.setName(rName);
-    receive.setAdd(rAdd);
-    receive.setCity(rCity);
-    receive.setState(rState);
-    receive.setZip(rZip);
-
-    string cost="6*5=30";
-    Person* sendPtr=new Person;
-    Person* receivePtr= new Person;
-    sendPtr=&send;
-    receivePtr=&receive;
-
-    labelFunc(send, receive, cost);
 }
